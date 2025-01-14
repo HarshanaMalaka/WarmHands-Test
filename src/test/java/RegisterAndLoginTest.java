44965c3909edf32e9c3fd2c813683a1ae1f0e953
Harshana Malaka <harshanamaduwanthasilva@gmail.com>
2025-01-14 18:06:07 +0530

    Adding rest of Registration Testing

--- src/test/java/RegisterAndLoginTest.java
+++ src/test/java/RegisterAndLoginTest.java
@@ -20,24 +20,69 @@ public class RegisterAndLoginTest {
 
     @Test
     public void RegisterLogin() throws InterruptedException {
+
+        //Register Page
+
         //01.click register button
         driver.findElement(By.xpath("//a[@class='link btn-a register']")).click();
         Thread.sleep(1000);
 
-        //Fill the form
+        //02 .Fill the form
+
+        //first name
         driver.findElement(By.xpath("//input[@name='fname']")).sendKeys("Test");
         Thread.sleep(500);
 
+        //last name
         driver.findElement(By.xpath("//input[@name='lname']")).sendKeys("User");
         Thread.sleep(500);
 
+        //address
         driver.findElement(By.xpath("//input[@name='address']")).sendKeys("19/C,Bandaragama Rd,Kaluthara");
         Thread.sleep(500);
 
-        WebElement dropdown =driver.findElement(By.xpath("//select[@name='province']"));
-        dropdown.click();
-        Select select = new Select(dropdown);
-        select.selectByVisibleText("Western");
+        //province
+        WebElement dropdownprovince =driver.findElement(By.xpath("//select[@name='province']"));
+        dropdownprovince.click();
+        Select selectprovince = new Select(dropdownprovince);
+        selectprovince.selectByVisibleText("Western");
+
+        //district
+        WebElement dropdowndistrict=driver.findElement(By.xpath("//select[@name='district']"));
+        dropdowndistrict.click();
+        Select selectdistrict = new Select(dropdowndistrict);
+        selectdistrict.selectByVisibleText("Kalutara");
+
+        //city
+        WebElement dropdowncity=driver.findElement(By.xpath("//select[@name='city']"));
+        dropdowncity.click();
+        Select selectcity = new Select(dropdowncity);
+        selectcity.selectByVisibleText("Panadura");
+
+        //nic
+        driver.findElement(By.xpath("//input[@name='nic']")).sendKeys("1234567890");
+
+        //phone number
+        driver.findElement(By.xpath("//input[@name='mobile']")).sendKeys("0771234568");
+
+        //03.click next button
+        driver.findElement(By.xpath("//button[@class='next-btn']")).click();
+
+        //email
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("testuserwestern@gmail.com");
+
+        //password
+        driver.findElement(By.xpath("//input[@placeholder='Enter Password']")).sendKeys("testpassword");
+        Thread.sleep(1000);
+
+        //confirm password
+        driver.findElement(By.xpath("//input[@placeholder='Enter Password again']")).sendKeys("testpassword");
+
+        //04.click register button
+        driver.findElement(By.xpath("//button[@class='register-btn']")).click();
+
+
+
     }
 
 
